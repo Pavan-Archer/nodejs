0dc8e1c2d4d81bd2f7886adc75c5e0599f55825a
Pavan Archer <pavanarcher18@gmail.com>
2024-09-12 00:59:31 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // const hello = 'Hello world!'
-// console.log(hello) 
+// console.log(hello)
+
+// Blocking Synchronous way
 
 // Read File
 
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn)
+console.log(textIn);
 
 // Create New File
 
 const textOut = `Reading the input file : ${textIn}`;
-fs.writeFileSync('./txt/output.txt', textOut);
+fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log('Its Working!')
+console.log("Its Working!");
+
+// Non Blocking Synchronous way
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) return console.log("ERROR!");
+  console.log(data);
+  fs.writeFile("./txt/final.txt", "utf-8", (err) => {
+    console.log("Your files has been written!");
+  });
+});
